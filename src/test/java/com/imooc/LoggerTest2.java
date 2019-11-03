b7cddb21c659eff3758dc6a402cacd66bcdd04f9
wuligang <wlggyh@163.com>
2019-11-03 17:59:19 +0800

    Merge branch 'master' into dev_wlg
    # Conflicts:
    #	src/test/java/com/imooc/LoggerTest2.java

--- src/test/java/com/imooc/LoggerTest2.java
+++ src/test/java/com/imooc/LoggerTest2.java
@@ -5,10 +5,7 @@ package com.imooc;
  * 2017-06-02 17:55
  */
 public class LoggerTest2 {
-//测试不出来啊啊啊
-
     public static void main(String[] args) {
-
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
