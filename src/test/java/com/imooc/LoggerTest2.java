92dae078c36bc690e7127cb7054a4bd2228a4211
wuligang <32867846+wuligang@users.noreply.github.com>
2019-11-03 18:00:14 +0800

    Update LoggerTest2.java

--- src/test/java/com/imooc/LoggerTest2.java
+++ src/test/java/com/imooc/LoggerTest2.java
@@ -6,9 +6,9 @@ package com.imooc;
  */
 public class LoggerTest2 {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
